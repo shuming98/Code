70c711047c1aed1f4acaa18b29b0cc4a87410f26
伍书铭 <1165296623@qq.com>
2018-10-10 22:49:39 +0800

    整合新版到包

---
+++ nglinux以前版本/nglinux1.0.3.0mini/img/home/_POST.php
@@ -0,0 +1,15 @@
+<?php  
+$conn=mysqli_connect('localhost','root','','nglinux');
+mysqli_query($conn,'set names utf8');
+$sql="insert into contact(name,contact,message) values ('$_POST[name]','$_POST[email]','$_POST[message]')";
+$res=mysqli_query($conn,$sql);
+if(!res){
+	echo '留言失败，请尝试再次留言';
+	exit();
+}
+else{
+	header('location:../../index.html');
+}
+mysqli_close($conn);
+
+?>
